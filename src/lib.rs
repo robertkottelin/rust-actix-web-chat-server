a8e009892e5de3db784d0f9d2f8d769c2fa0a178
robertkottelin <robert.kottelin@gmail.com>
2023-04-30 12:18:39 +0200

    prototype working

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+// lib.rs
+
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::{Deserialize, Serialize};
+use std::net::TcpListener;
+use std::sync::{Arc, Mutex};
+
+type MessageMap = Arc<Mutex<std::collections::HashMap<String, String>>>;
+
+#[derive(Deserialize, Serialize)]
+struct SendMessage {
+    user: String,
+    message: String,
+}
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn post_message(
+    send_message: web::Json<SendMessage>,
+    message_map: web::Data<MessageMap>,
+) -> HttpResponse {
+    let mut map = message_map.lock().unwrap();
+    map.insert(send_message.user.clone(), send_message.message.clone());
+    HttpResponse::Ok().body(format!(
+        "Message from {}: {}",
+        send_message.user, send_message.message
+    ))
+}
+
+async fn read_messages(message_map: web::Data<MessageMap>) -> HttpResponse {
+    let map = message_map.lock().unwrap();
+    let messages: Vec<String> = map
+        .iter()
+        .map(|(user, message)| format!("{}: {}", user, message))
+        .collect();
+
+    HttpResponse::Ok().json(messages)
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let message_map: MessageMap = Arc::new(Mutex::new(std::collections::HashMap::new()));
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(message_map.clone()))
+            .route("/health_check", web::get().to(health_check))
+            .route("/send", web::post().to(post_message))
+            .route("/read", web::get().to(read_messages))
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// main.rs
+
+use chat::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
+}
